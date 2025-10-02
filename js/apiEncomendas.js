cffcd9a07ce04bb298f60435eadba8fd18847937
Mirela Viana <vianamirela.melo@gmail.com>
2025-10-01 22:24:47 -0300

    Veiculação da API no site principal
    Botão para add as informação da API no formulário de encomendas.

---
+++ js/apiEncomendas.js
@@ -0,0 +1,16 @@
+var botãoAPI = document.querySelector("#apiEncomenda")
+
+botãoAPI.addEventListener("click", function(){
+    var conectaAPI = new XMLHttpRequest()
+    conectaAPI.open("GET", "http://localhost:3000/encomendas_web") //Conectar com a API
+
+    conectaAPI.addEventListener("load", function(){
+        var respostaAPI = conectaAPI.responseText
+        var novaEncomenda = JSON.parse(respostaAPI)
+        
+        novaEncomenda.forEach(function(cadaEncomenda){
+            addEncomendaTabela(cadaEncomenda)
+        })
+    })
+    conectaAPI.send()
+})
