b5df91b2dafeec34bd7aa94ef35ea646a6244791
Mirela Viana <vianamirela.melo@gmail.com>
2025-11-26 14:11:13 -0300

    Correção do formulário para add encomendas
    Antes ele não identificava o valor e o total, agora esta certo.

--- js/formAdicionaEncomendas.js
+++ js/formAdicionaEncomendas.js
@@ -2,59 +2,47 @@ var botaAdicionar = document.querySelector("#enviar_form");
 
 botaAdicionar.addEventListener("click", function(event) {
     event.preventDefault();
-    // Aqui você pode adicionar a lógica para enviar o formulário
-    //console.log("O botão foi clicado");
 
-    //Captura o formulario de encomendas
-    var form = document.querySelector("#formencomendas")
+    var form = document.querySelector("#formencomendas");
 
-    //Captura os dados da nova encomenda
-    var encomendaNova = obtemEncomenda(form)
+    var encomendaNova = obtemEncomenda(form);
 
-    //insere a encomenda na tabela
-    addEncomendaTabela(encomendaNova)
+    addEncomendaTabela(encomendaNova);
 
-    //limpar form
-    form.reset()
+    form.reset();
 });
 
 function addEncomendaTabela(novaEncomenda){
-    //captura a tabela de encomedas
-    var tabela = document.querySelector(".tabela-cliente")
-
-    //Cria a nova linha na tabela
-    tabela.appendChild(montaTR(novaEncomenda))
+    var tabela = document.querySelector(".tabela-cliente");
+    tabela.appendChild(montaTR(novaEncomenda));
 }
 
 function obtemEncomenda(formulario){
-
     var encomenda = {
         nome: formulario.nome.value,
         produto: formulario.produto.value,
         qtde: formulario.qtdP.value,
-        unitario: formulario.valorP.value
+        unitario: formulario.valorP.value  // <-- este é o nome correto
     }
     return encomenda;
 }
 
-// cria linha
 function montaTR(dadosEncomenda){
-    var tr = document.createElement("tr")
+    var tr = document.createElement("tr");
 
-    tr.appendChild(montaTD(dadosEncomenda.nome))
-    tr.appendChild(montaTD(dadosEncomenda.produto)) 
-    tr.appendChild(montaTD(formataValor(parseFloat(dadosEncomenda.valorUnitario))))
-    tr.appendChild(montaTD(dadosEncomenda.qtde))   
-    tr.appendChild(montaTD(calcularTotal(dadosEncomenda.qtde, dadosEncomenda.valorUnitario)))
+    tr.classList.add("cliente"); // IMPORTANTE para o cálculo posterior
 
-    return tr
+    tr.appendChild(montaTD(dadosEncomenda.nome));
+    tr.appendChild(montaTD(dadosEncomenda.produto));
+    tr.appendChild(montaTD(formataValor(parseFloat(dadosEncomenda.unitario)))).classList.add("valor-unitario");
+    tr.appendChild(montaTD(dadosEncomenda.qtde)).classList.add("qtde");
+    tr.appendChild(montaTD(calcularTotal(dadosEncomenda.qtde, dadosEncomenda.unitario))).classList.add("valor-total");
+
+    return tr;
 }
 
-
-//cria coluna
 function montaTD(dado){
-    var td = document.createElement("td")
-    td.textContent = dado
-    return td
+    var td = document.createElement("td");
+    td.textContent = dado;
+    return td;
 }
-
